bc64747e75fe4575352210d8a3b9876b7ff5094e
Dinislam Turaykhanov <116585464+7nmw@users.noreply.github.com>
2025-02-04 12:10:53 +0300

    Фильтры и пагинация

--- NewsPaper/urls.py
+++ NewsPaper/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from news.views import PostSearch
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('pages/', include('django.contrib.flatpages.urls')),
-   path('news/', include('news.urls')),  # < вот тут
+   path('news/', include('news.urls')),
+   path('article/', include('news.urls')), # < вот тут
+   path('news/search/', PostSearch.as_view(), name='search'),
 ]
